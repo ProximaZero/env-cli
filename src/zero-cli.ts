037ff9904581c4503941ee4963e4dcbebd411a09
plinho <>
2020-11-04 18:17:11 -0300

    zero js script files

--- src/zero-cli.ts
+++ src/zero-cli.ts
@@ -3,8 +3,17 @@ import * as prompts from 'prompts';
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 
-export class ZeroCli {
+export class ZeroCli extends PromptEloquent {
     async main() {
+        console.log('Zero Cli Startup');
+    }
+
+    async initDevMode(){
+        console.log('...startting devmode');
+
+    }
+
+    async fix(arguments?: any){
 
     }
 }
