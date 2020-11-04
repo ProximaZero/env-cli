1f57741a69ab33b1cac770cf487ecc449b64e984
plinho <>
2020-11-04 18:16:44 -0300

    fixing zero-cli commands

---
+++ src/zero-cli.ts
@@ -0,0 +1,11 @@
+import * as Crypto from 'crypto-js';
+import * as prompts from 'prompts';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+export class ZeroCli {
+    async main() {
+
+    }
+}
+(async () => new ZeroCli().main())().then();
